68caf801342b7c606d3c24d371e8947e836ac82f
srashtisj <srashti0204@gmail.com>
2021-09-23 11:13:35 +0530

    Initial commit

---
+++ src/app/button-overview-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'button-overview-example',
+  templateUrl: 'button-overview-example.html',
+  styleUrls: ['button-overview-example.css'],
+})
+export class ButtonOverviewExample {}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
